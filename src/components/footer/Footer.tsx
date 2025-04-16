3103f47b32f0d4ac6c9882e0d99310c5b50067b8
Isabela Santos <isantos.ipereira@gmail.com>
2025-04-16 16:37:30 -0300

    ajuste

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -12,7 +12,6 @@ function Footer() {
                     </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-2'>
-                        // alterar links depois
                         <a href="https://www.linkedin.com/in/seu_usuario" target="_blank">
     	                    <LinkedinLogo size={48} weight='bold' />
                         </a>
